8c20bb820855385d4a352dbc36715981f4387b4b
Gustavo de Barros Feitosa <Gustavo@boavista.net>
2023-05-24 15:01:15 -0300

    Criação de rotas de cadastro/login

--- routes.js
+++ routes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const cadastroController = require('./src/controllers/cadastroController');
 
 // Rotas da home
-route.get('/', homeController.paginaInicial);
+route.get('/', homeController.index);
+
+//Rotas de Login
+route.get('/login',loginController.index);
+
+//Rotas de cadastro
+route.get("/cadastro",cadastroController.index)
 
 
 
